aa0e717385701985dc0697f5dd41d2a525adaac0
sailboatlarry@gmail.com <sailboatlarry@gmail.com>
2018-05-06 15:21:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,4 @@ function deleteFromObjectByKey(object, key){
   delete object[key]
   return object
 }
-function deleteFromObjectByKey(object, key){
-  var newObject = object
-  newObject.assign({}, { key: 0 })
-  delete newObject[key]
-  return newObject
-}
+
