c2f07fc50172a073e255ba94b784ab35990677df
sailboatlarry@gmail.com <sailboatlarry@gmail.com>
2018-05-06 14:14:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value});  
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, val
+ue){
+  object[key] = value;
+  return object
+}
